7823f61e21162b8b44e04983e4786a87df29789e
Bhavesh chavda <bjchavda746525@gmail.com>
2025-03-09 17:43:46 +0530

    final commit

--- app/src/main/java/com/example/socialmedia/MainActivity.kt
+++ app/src/main/java/com/example/socialmedia/MainActivity.kt
@@ -2,27 +2,63 @@ package com.example.socialmedia
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.socialmedia.Model.Post
+import com.example.socialmedia.daos.PostDao
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.firestore.Query
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), IPostAdapter {
 
     private lateinit var createPost: FloatingActionButton
+    lateinit var postDao: PostDao
+    private lateinit var adapter: PostAdapter
+    private lateinit var postRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         createPost = findViewById(R.id.addPostBtn)
+        postRecyclerView = findViewById(R.id.post_recycle)
+        postDao = PostDao()
 
         createPost.setOnClickListener {
             val addPost = Intent(this,CreatePostActivity::class.java)
             startActivity(addPost)
         }
 
+        setUpRV()
+
+    }
+
+    private fun setUpRV() {
+
+        val postsCollections = postDao.postCollection
+        val query = postsCollections.orderBy("createdAt", Query.Direction.DESCENDING)
+        val recyclerViewOptions = FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java).build()
+
+        adapter = PostAdapter(recyclerViewOptions,this)
+
+
+        postRecyclerView.adapter = adapter
+        postRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.stopListening()
+    }
+
+    override fun onLikeClicked(postId: String) {
+        postDao.UpdateLikes(postId)
     }
 }
--- app/src/main/java/com/example/socialmedia/daos/PostDao.kt
+++ app/src/main/java/com/example/socialmedia/daos/PostDao.kt
@@ -3,7 +3,9 @@ package com.example.socialmedia.daos
 
 import com.example.socialmedia.Model.Post
 import com.example.socialmedia.Model.User
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.GlobalScope
@@ -17,7 +19,7 @@ class PostDao {
     val auth = Firebase.auth
 
 
-    fun AddPost(text: String){
+    fun AddPost(text: String) {
 
         val currentUserId = auth.currentUser!!.uid
 
@@ -26,10 +28,30 @@ class PostDao {
             val user = userDao.getUserById(currentUserId).await().toObject(User::class.java)!!
 
             val currentTime = System.currentTimeMillis()
-            val post = Post(text,user,currentTime)
+            val post = Post(text, user, currentTime)
             postCollection.document().set(post)
         }
 
+
+    }
+
+    fun getPostById(postId: String): Task<DocumentSnapshot> {
+        return postCollection.document(postId).get()
+    }
+
+    fun UpdateLikes(postId: String) {
+        GlobalScope.launch {
+            val currentUserId = auth.currentUser!!.uid
+            val post = getPostById(postId).await().toObject(Post::class.java)!!
+            val isLiked = post.likedBy.contains(currentUserId)
+
+            if (isLiked) {
+                post.likedBy.remove(currentUserId)
+            } else {
+                post.likedBy.add(currentUserId)
+            }
+            postCollection.document(postId).set(post)
+        }
 
 
     }
